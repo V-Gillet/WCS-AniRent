5f72ab37db8e8c3b5c5a4a8e74e5855439747d03
V-Gillet <gillet.valentin45000@gmail.com>
2022-11-24 14:08:25 +0100

    changed API

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,7 @@
-var year = 1805
+var name = 'spider'
 $.ajax({
     method: 'GET',
-    url: `https://api.api-ninjas.com/v1/historicalevents?year=${year}`,
+    url: 'https://api.api-ninjas.com/v1/animals?name=' + name,
     headers: { 'X-Api-Key': 'eR3VDpbLcqcCOXn13DKSmQ==jJFgr2OWbq9Kb8AC'},
     contentType: 'application/json',
     success: function(result) {
