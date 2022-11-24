cc42928cb81492392b3aa1a58a9e20431ba5ceb3
hhertout <hugues.hertout@gmail.com>
2022-11-24 19:50:42 +0100

    update

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,13 +2,68 @@
 
 namespace App\Controller;
 
+use App\Model\AnimalApi;
+
 class AnimalController extends AbstractController
 {
-    /**
-     * Display home page
-     */
+    protected const ANIMALS = [
+        'Jumping Spider',
+        'Llama',
+        'Czechoslovakian Wolfdog',
+        'Leopard Cat',
+        'Turtle',
+        'Giraffe',
+        'Snake',
+        'kangaroo',
+        'Squirrel',
+    ];
+    protected const FLYER = [
+        'Mosquito',
+        'Sea Eagle',
+        'Snowy Owl',
+    ];
+
     public function index(): string
     {
-        return $this->twig->render('Animal/listAnimals.html.twig');
+        $animals = $flyers = [];
+        foreach (self::ANIMALS as $animal) {
+            $allAnimals[] = $this->getAnimals($animal);
+        }
+        foreach ($allAnimals as $animal) {
+            $animals[] = $this->getCaracteristic($animal);
+        }
+        foreach (self::FLYER as $flyer) {
+            $allFlyers[] = $this->getAnimals($flyer);
+        }
+        foreach ($allFlyers as $flyer) {
+            $flyers[] = $this->getCaracteristic($flyer);
+        }
+        $animals = array_merge($flyers, $animals);
+
+        return $this->twig->render('Animal/listAnimals.html.twig', [
+            'animals' => $animals
+        ]);
+    }
+    public function getAnimals($animal)
+    {
+        $animalApiManager = new AnimalApi();
+        return $animalApiManager->getAnimals($animal);
+    }
+    public function getCaracteristic($animals)
+    {
+        $animalToRent = [];
+
+        foreach ($animals as $animal) {
+            $animal['characteristics']['top_speed'] = $animal['characteristics']['top_speed'] ?? '';
+            if ($animal['characteristics']['top_speed'] !== '') {
+                $name = $animal['name'];
+                $characteristics = $animal['characteristics']['top_speed'];
+                $animalToRent[] = [
+                    'name' => $name,
+                    'characteristics' => $characteristics,
+                ];
+            }
+        }
+        return $animalToRent[0];
     }
 }
