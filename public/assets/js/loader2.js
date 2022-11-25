8c2b6cc9d20f034ab2feaf0b07a92f73ffb00869
hhertout <hugues.hertout@gmail.com>
2022-11-25 11:52:01 +0100

    fix

---
+++ public/assets/js/loader2.js
@@ -0,0 +1,6 @@
+let buttonLoader2 = document.getElementById('submit-and-load2')
+const loaderContainer = document.getElementById('loader-container')
+
+buttonLoader2.addEventListener('click', ()=> {
+    loaderContainer.style.display = 'block'
+})
