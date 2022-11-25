16ebf21ed6cabd422d96fc595f07879381fc5381
V-Gillet <gillet.valentin45000@gmail.com>
2022-11-25 04:26:03 +0100

    unset session on confirmation

--- src/Controller/ConfirmController.php
+++ src/Controller/ConfirmController.php
@@ -8,6 +8,7 @@ class ConfirmController extends AbstractController
 {
     public function index(): string
     {
+        session_unset();
         return $this->twig->render('Reserve/confirmation.html.twig');
     }
 }
