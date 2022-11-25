74c790f3259988bd18540a88f10e241ee834163c
hhertout <hugues.hertout@gmail.com>
2022-11-25 12:11:31 +0100

    fix

--- public/assets/js/loader2.js
+++ public/assets/js/loader2.js
@@ -1,6 +1,16 @@
 let buttonLoader2 = document.getElementById('submit-and-load2')
-const loaderContainer2 = document.getElementById('loader-container2')
 
-buttonLoader2.addEventListener('click', ()=> {
+window.addEventListener('load', ()=> {
+    let loaderContainer2 = document.getElementById('loader-container2')
+
+    console.log(loaderContainer2)
+
+    buttonLoader2.addEventListener('click', ()=> {
     loaderContainer2.style.display = 'block'
 })
+})
+
+
+
+
+
