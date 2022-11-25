004d81a8646a137a02902ee40933923c4b70b1af
hhertout <hugues.hertout@gmail.com>
2022-11-25 12:48:13 +0100

    fix loader

--- public/assets/js/loader2.js
+++ public/assets/js/loader2.js
@@ -1,13 +1,15 @@
-let buttonLoader2 = document.getElementById('submit-and-load2')
+let buttonLoader2 = document.querySelectorAll('#submit-and-load2')
 
 window.addEventListener('load', ()=> {
     let loaderContainer2 = document.getElementById('loader-container2')
 
     console.log(loaderContainer2)
 
-    buttonLoader2.addEventListener('click', ()=> {
-    loaderContainer2.style.display = 'block'
-})
+    buttonLoader2.forEach( button => {
+        button.addEventListener('click', ()=> {
+            loaderContainer2.style.display = 'block'
+        })
+    })
 })
 
 
