5ba17819b167be143692b8188dd81edf30320ade
hhertout <hugues.hertout@gmail.com>
2022-11-25 11:20:01 +0100

    anim

--- public/assets/js/loader.js
+++ public/assets/js/loader.js
@@ -1,48 +1,14 @@
-$(document).ready(function() {
-    let counter = 0;
+const buttonLoader = document.getElementById('submit-and-load')
+const loaderContainer = document.getElementById('loader-container')
+
+
+buttonLoader.addEventListener('click', ()=> {
+    loaderContainer.style.display = 'block'
+})
+
+
+
+
+
   
-    // Start the changing images
-    setInterval(function() {
-      if(counter == 9) { 
-        counter = 0; 
-      }
   
-      changeImage(counter);
-      counter++;
-    }, 3000);
-  
-    // Set the percentage off
-    loading();
-  });
-  
-  function changeImage(counter) {
-    let images = [
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">',
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">',
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">',
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">',
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">',
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">',
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">',
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">',
-      '<img src="/assets/images/lama.jpeg" alt="image de loader">'
-    ];
-  
-    $(".loader .image").html(""+ images[counter] +"");
-  }
-  
-  function loading(){
-    let num = 0;
-  
-    for(i=0; i<=100; i++) {
-      setTimeout(function() { 
-        $('.loader span').html(num+'%');
-  
-        if(num == 100) {
-          loading();
-        }
-        num++;
-      },i*120);
-    };
-  
-  }
